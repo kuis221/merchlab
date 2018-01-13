a50588ba593beb4d3040a2f7cd6adecf3cfb412e
Jeffrey Huang <jeffdh5@gmail.com>
2018-01-12 17:53:59 -0800

    show up to 10m

--- app.py
+++ app.py
@@ -1111,7 +1111,7 @@ def execute_query_search_v2(query):
 		and lower(asin_metadata.title) like '%%{}%%'
 		""".format(scrubbed_query.lower())
 		negative_queries_sql = construct_negative_queries(query)
-		salesrank_threshold = 20000000
+		salesrank_threshold = 10000000
 	else:
 		salesrank_threshold = 1000000
 
