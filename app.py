ea2658a98dc8b00671cbe66ded7517b2c47990ae
Jeffrey Huang <jeffdh5@gmail.com>
2018-02-11 01:02:18 -0800

    filter out consistent winners with high bsr

--- app.py
+++ app.py
@@ -657,7 +657,7 @@ def get_trending_tshirts_by_metric(metric, query=None, asc=False, filter_zeroes=
 	FROM asin_analytics 
 	INNER JOIN asin_metadata ON asin_analytics.id=asin_metadata.id
 	and asin_analytics.unthrottled_salesrank > 0 and asin_analytics.list_price > 0
-	and asin_analytics.unthrottled_salesrank < 2000000
+	and asin_analytics.unthrottled_salesrank < 300000
 	and asin_metadata.product_type_name='ORCA_SHIRT'
 	{}
 	and asin_analytics.last_indexed_date > '{}'
